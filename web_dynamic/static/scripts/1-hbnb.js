1c62092bf25c774ef3bb0307665547dd7de032ff
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-13 22:56:56 +0300

    jQuery script to show selected amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,5 +1,17 @@
 $( document ).ready(function(){
-    if($("DIV.amenities data-id").checked){
-        alert("Ermias!")
-    }
+    let checked = {};
+    $('input[type="checkbox"]').click(function() {
+        if($(this).prop("checked") == true){
+            checked[$(this).attr('data-id')] = $(this).attr('data-name');
+        }
+        else if($(this).prop("checked") == false) {
+            delete checked[$(this).attr('data-id')];
+        }
+        if (Object.keys(checked).length > 0) {
+            $('.amenities h4').text(Object.values(checked).join(', '));
+        }
+        else {
+            $('.amenities h4').html('&nbsp;');
+        }
+    });
 });
